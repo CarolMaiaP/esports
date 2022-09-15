d895e0bcda9a5ad18ad26991af6a816fa533d8b0
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-09-15 18:05:06 -0300

    react native button component created

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+interface ButtonProps{
+  title: string,
+}
+
+function Button(props: ButtonProps){
+  return(
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        Hello React Native!
+        Helloo React Native!
       </Text>
+      <Button title="Send 1"/>
+      <Button title="Send 2"/>
+      <Button title="Send 3"/>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,13 +32,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 
   title: {
-    color: '#fff',
+    color: '#000',
     fontSize: 22,
-  }
+  },
 });
