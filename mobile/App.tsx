bb0995420bd663bf4c42b8af0712c4e24654d65f
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-09-16 18:10:23 -0300

    background mobile image added

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,43 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-
-interface ButtonProps{
-  title: string,
-}
-
-function Button(props: ButtonProps){
-  return(
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
+import { Background } from './src/components/Background';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        Helloo React Native!
-      </Text>
-      <Button title="Send 1"/>
-      <Button title="Send 2"/>
-      <StatusBar style="auto" />
-    </View>
+     <Background>
+  
+     </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    color: '#000',
-    fontSize: 22,
-  },
-});
