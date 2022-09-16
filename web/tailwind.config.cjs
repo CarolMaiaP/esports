4c25d5e0c213c5b2cf3cc213f89420cd0d7af252
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-09-16 12:18:40 -0300

    home page initialized

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.tsx"],
+  content: ["./src/**/*.tsx", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        "nlw-gradient":
+          "linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 76.94%, #E1D55D 1.57%)",
+      },
+    },
   },
   plugins: [],
 };
