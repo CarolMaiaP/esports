d8915fe6682eefbffb307874a706ca94f513ee35
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-09-22 14:39:52 -0300

    Ad listing added

--- server/src/server.ts
+++ server/src/server.ts
@@ -23,17 +23,30 @@ app.post('/ads', (request, response) => {
   return response.status(201).json([]);
 })
 
-app.get("/games/:id/ads", (request, response) => {
-  // const gameId = request.params.id;
+app.get("/games/:id/ads", async(request, response) => {
+  const gameId = request.params.id;
 
-  // return response.send(gameId)
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true,
+    },
+    
+    where: {
+      gameId,
+    },
+
+    orderBy: {
+      createdAt: 'desc',
+    }
+  })
   
-  return response.json([
-    { id: 1, name: "Anuncio 1" },
-    { id: 2, name: "Anuncio 2" },
-    { id: 3, name: "Anuncio 3" },
-    { id: 4, name: "Anuncio 4" },
-  ]);
+  return response.json(ads);
 });
 
 app.get("/ads/:id/discord", (request, response) => {
