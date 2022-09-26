3a384d0036b8b930370cec48dce6dda76a4dbc05
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-09-26 12:13:01 -0300

    dialog component with radix-ui added

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,9 @@
+import { useEffect, useState } from 'react'
+import * as Dialog from '@radix-ui/react-dialog'
 import { GameBanner } from './components/GameBanner'
 import './styles/main.css'
 import logoImg from './assets/logo-nlw-esports.svg'
 import { CreateAdBanner } from './components/CreateAdBanner'
-import { useEffect, useState } from 'react'
 
 interface Game{
   id: string;
@@ -42,10 +43,23 @@ function App() {
               adsCount={game._count.ads}/>
           )
         })}
-        
       </div> 
 
-      <CreateAdBanner/>
+      <Dialog.Root>
+        <CreateAdBanner/>
+
+        <Dialog.Portal>
+          <Dialog.Overlay className='bg-black/80 inset-0 fixed' />
+
+          <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2'>
+            <Dialog.Title>Publique um anúncio</Dialog.Title>
+
+            <Dialog.Content>
+              shiudhsuayshdigjgoihjoigjhjgoihgj
+            </Dialog.Content>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   )
 }
