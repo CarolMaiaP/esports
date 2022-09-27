4400fe3f66b14796da341c8f35970d0cab9a3340
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-09-27 15:01:21 -0300

    form style added

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -50,16 +50,21 @@ function App() {
         <CreateAdBanner/>
 
         <Dialog.Portal>
-          <Dialog.Overlay className='bg-black/80 inset-0 fixed' />
+          <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
 
-          <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
+          <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
             <Dialog.Title className='text-3xl font-black'>Publique um anúncio</Dialog.Title>
 
             <Dialog.Content>
-              <form>
-                <div>
-                  <label htmlFor="game">Qual o game?</label>
-                  <input id='game' type="text" placeholder='Selecione o game que deseja jogar' />
+              <form className='mt-8'>
+                <div className='flex flex-col gap-2'>
+                  <label htmlFor="game" className='font-semibold'>Qual o game?</label>
+                  <input 
+                    id='game' 
+                    type="text" 
+                    placeholder='Selecione o game que deseja jogar' 
+                    className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'  
+                  />
                 </div>
 
                 <div>
@@ -85,14 +90,14 @@ function App() {
                     <label htmlFor="hourStart">Qual horário do dia?</label>
                     <div>
                       <input id='hourStart' type="time" placeholder='De'/>
-                      <input id="hourEnd" type="time" placeholder='Até'/>
+                      <input id='hourEnd' type="time"  placeholder='Até'/>
                     </div>
                   </div>
                 </div>
 
                 <div>
                   <input type="checkbox" />
-                  Costume me conectar ao chat de voz
+                  Costumo me conectar ao chat de voz
                 </div>
 
                 <footer>
